3110d1895c96a88eede0853af5b490d55a3be16c
Muneeb <muneeb.softblock@gmail.com>
2022-11-28 16:11:26 +0500

    2 jwks

--- server-auth-jwt.js
+++ server-auth-jwt.js
@@ -22,6 +22,21 @@ MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAPMnQBFsUMxP05b8qkm7MhUFLTNFc6fv
 NxxUTjGNVHL2pY3a3f0drwnhUPKzMEdUR9d8RGEqX8l0wyfflSYnFRcCAwEAAQ==
 -----END PUBLIC KEY-----
 `;
+const privateKey2 = `-----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJBALXsC7V5AqwQ8RmJIKnbzCiyR87Z03PBpXFUgSd1HpHkQjREhYcH
+F5oOQLTipPmZvu8hj+PdBHEv/rlXAyod/X8CAwEAAQJAOydbz1Bf3A9ONl1ub4z2
+36zIyKvvo84tu6IctPXSUOF4qR0orcfBPtTqRnm+I78Cmy4AlO6j2rI6XvyTU66f
+AQIhAPENz//jo90sPy89OLJl2e/YFJizz7hHMYs+hwcxA9fBAiEAwTOl05F1c60w
+MXQn4sxLFcXoFo4HL3XqfxgHbuM9JT8CIQDvyPWnbiKK2IDh1NyZWYe5dhDG9dcj
+UG6QPNrE5JUWAQIgOC6+wEGCeDsa0qbGmotyIkjE7xoqOMD/iioAr1xhgZ0CIBIn
+O1T08OL6pXWXAORJAc94Yc7Fp/8cgtFOu6EhalxJ
+-----END RSA PRIVATE KEY-----
+`;
+const publicKey2 = `-----BEGIN PUBLIC KEY-----
+MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALXsC7V5AqwQ8RmJIKnbzCiyR87Z03PB
+pXFUgSd1HpHkQjREhYcHF5oOQLTipPmZvu8hj+PdBHEv/rlXAyod/X8CAwEAAQ==
+-----END PUBLIC KEY-----
+`;
 
 app.set('json spaces', 2);
 
@@ -34,7 +49,10 @@ app.get('/', (r, res) => res.json({ message: 'hi ' + Date() }));
 app.get('/.well-known/jwks.json', async (req, res) => {
   try {
     res.status(200).json({
-      keys: [rsaPemToJwk(privateKey, { use: 'sig', alg: 'RS256', keyid: 'xxUTjGNVHL2pY3a3f0drwnhUPKzMEdUR9d8R' }, 'public')],
+      keys: [
+        rsaPemToJwk(privateKey, { use: 'sig', alg: 'RS256', keyid: 'xxUTjGNVHL2pY3a3f0drwnhUPKzMEdUR9d8R' }, 'public'),
+        rsaPemToJwk(privateKey2, { use: 'sig', alg: 'RS256', keyid: '3a3f0drwnhUPKzMExxUTjGNVHL2pYdUR9d8R' }, 'public'),
+      ],
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
